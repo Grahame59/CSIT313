8fe3a5804fb870965010f3995d3494425aa84ecb
Kyler Grahame <Kjgbusiness8@gmail.com>
2024-10-06 01:24:27 -0500

    Initial Commit

---
+++ homework_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+/*Steps to run:
+1. docker run -v C:\csit_313:/csit_313 -e DOTNET_CLI_HOME=/tmp --rm -it docker.io/bitnami/dotnet-sdk
+2. cd /csit_313/homework_1
+3. dotnet build
+4. dotnet run
+
+
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
